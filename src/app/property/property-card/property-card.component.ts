a75c11b1d2faaf841a628b37e8cc1b1164bf91f6
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-02-02 10:54:24 +0530

    properties service done

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProperty } from '../property-list/Iproperty.interface';
 
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
-  styleUrls: ['./property-card.component.css']
+  styleUrls: ['./property-card.component.css'],
 })
 export class PropertyCardComponent implements OnInit {
-
-  property:any ={
-    Id:1,
-    Name:"ABCD",
-    Type:"House",
-    Price:12000
-  }
-  constructor() { }
+  @Input() property!:IProperty
+  constructor() {}
 
   ngOnInit(): void {
-  }
 
+  }
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from './Iproperty.interface';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-
-  constructor() { }
+  properties!: Array<IProperty>;
+  constructor(private housing: HousingService) {}
 
   ngOnInit(): void {
+    this.housing
+      .getAllProperties()
+      .subscribe((data) => (this.properties = data),error =>{
+          console.log(error)
+      });
   }
-
 }
