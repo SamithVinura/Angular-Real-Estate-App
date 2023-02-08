b24fb1d55dcebf7c4dc1db650159dce3af4dbdc5
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-02-08 08:53:24 +0530

    logout function done

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AlertyfyService } from 'src/app/services/alertyfy.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -10,7 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor(private auth:AuthService,private alertyfy:AlertyfyService) { }
+  constructor(private auth:AuthService,private alertyfy:AlertyfyService,private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -21,6 +22,7 @@ export class UserLoginComponent implements OnInit {
     if(token){
       localStorage.setItem('token',token.userName)
       this.alertyfy.success('Login Success')
+      this.router.navigate(['/'])
     }else{
       this.alertyfy.error('Username or Password is invalid')
     }
